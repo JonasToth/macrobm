8b11261eb878b17e68d7bc0f29b40e9b6fbab238
Jonas Toth <jonas.toth@gmail.com>
2017-02-04 19:09:51 +0100

    [Misc] inital commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// this program is about "macro benchmarking", in the sense that it will measure whole programm
+// execution and is able to compare run times.
+// Provide shell scripts that shall be measured!
+//
+fn main() {
+    println!("Hello, world!");
+}
