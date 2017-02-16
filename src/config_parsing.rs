c1a3bd87163045b6826eb5dcb03d93ac1360bd28
Jonas Toth <jonas.toth@gmail.com>
2017-02-16 13:10:56 +0100

    [Feature] simple parsing for argument list from configuration

---
+++ src/config_parsing.rs
@@ -0,0 +1,19 @@
+/// We parse configs cause we are so nice
+
+use yaml_rust::Yaml;
+
+pub fn yaml_args_to_stringlist(args: &Vec<Yaml>) -> Vec<String>
+{
+    let mut result: Vec<String> = Vec::new();
+
+    for arg_candidate in args {
+        match arg_candidate {
+            &Yaml::Real(ref a)   => result.push(a.clone()),
+            &Yaml::Integer(ref a)    => result.push(a.to_string()),
+            &Yaml::String(ref a) => result.push(a.clone()),
+            _       => panic!("Wrong argument type passed for that command!"),
+        }
+    }
+
+    result
+}
