6ab2b79816f71ec6ec44928b8d1950d534bb98cb
Jonas Toth <jonas.toth@gmail.com>
2017-02-20 13:24:55 +0100

    [Refactor] move test to bottom of file

--- src/statistics.rs
+++ src/statistics.rs
@@ -56,6 +56,69 @@ pub fn process_results(run_statistic: &BTreeMap<String, Vec<f32>>) -> BTreeMap<S
     result
 }
 
+/// Compare two runs of the same benchmark against each other and store which one one (with the
+/// given percantage of tolerance for equality).
+pub fn compare_runs(run1: &BTreeMap<String, BMStatistics>, run2: &BTreeMap<String, BMStatistics>, tolerance: f64) -> BTreeMap<String, ComparisonResult> {
+    let mut result = BTreeMap::new();
+    
+    for bm_name in run1.keys() {
+        let stat1 = run1.get(bm_name).unwrap();
+        let stat2 = match run2.get(bm_name) {
+            Some(stats) => stats,
+            None => continue,
+        };
+        let best_min = compare_single(stat1.min, stat2.min, tolerance);
+        let best_max = compare_single(stat1.max, stat2.max, tolerance);
+        let best_avg = compare_single(stat1.avg, stat2.avg, tolerance);
+
+        result.insert(bm_name.to_string(), ComparisonResult {
+            avg: best_avg,
+            min: best_min, max: best_max,
+        });
+    }
+
+    result
+}
+
+/// Compare two metrics for equality, tol(0. - 100.) is given in percent!
+fn compare_single(value1: f64, value2: f64, tol: f64) -> Comparison {
+    assert!(value1 != 0.);
+    if (value1 - value2).abs() / value1.abs() <= (tol / 100.) { return Comparison::Equal; }
+    else if value1 < value2 { return Comparison::OneIsFaster; }
+    else { return Comparison::TwoIsFaster; }
+}
+
+/// Calculate the procentual variance for that case. 100. * stddev / avg
+pub fn calc_relative_variance(statistics: &BMStatistics) -> f64 {
+    100. * statistics.dev / statistics.avg
+}
+
+/// Read in a result file and return all execution times mapped to their command name.
+/// Panics if the file is not existing or the yaml cant be loaded.
+pub fn read_result_from_file(file_name: &str) -> BTreeMap<String, Vec<f32>> {
+    let yml = file_to_yaml(file_name);
+    let yml = &yml[0];
+    let mut result = BTreeMap::new();
+
+    for single_result in yml.as_vec().unwrap() {
+        let single_result = single_result.as_hash().unwrap();
+
+        for (name, times) in single_result {
+            let mut times_float = Vec::new();
+
+            for el in times.as_vec().unwrap() {
+                times_float.push(el.as_f64().unwrap() as f32);
+            }
+            result.insert(name.as_str().unwrap().to_string(), times_float);
+        }
+    }
+
+    result
+}
+
+
+// ------------------------------- All tests for the statistic functionality -------------------
+
 #[test]
 fn test_process_results() {
     let mut collected_times = BTreeMap::new();
@@ -79,31 +142,6 @@ fn test_process_results_invalid() {
     let mut collected_times = BTreeMap::new();
     collected_times.insert("simulation".to_string(), Vec::<f32>::new());
     process_results(&collected_times);
-}
-
-/// Compare two runs of the same benchmark against each other and store which one one (with the
-/// given percantage of tolerance for equality).
-pub fn compare_runs(run1: &BTreeMap<String, BMStatistics>, run2: &BTreeMap<String, BMStatistics>, tolerance: f64) -> BTreeMap<String, ComparisonResult> {
-    let mut result = BTreeMap::new();
-    
-    for bm_name in run1.keys() {
-        let stat1 = run1.get(bm_name).unwrap();
-        let stat2 = match run2.get(bm_name) {
-            Some(stats) => stats,
-            None => continue,
-        };
-        let best_min = compare_single(stat1.min, stat2.min, tolerance);
-        let best_max = compare_single(stat1.max, stat2.max, tolerance);
-        let best_avg = compare_single(stat1.avg, stat2.avg, tolerance);
-
-        result.insert(bm_name.to_string(), ComparisonResult {
-            avg: best_avg,
-            min: best_min,
-            max: best_max,
-        });
-    }
-
-    result
 }
 
 #[test]
@@ -182,14 +220,6 @@ fn test_compare_runs() {
                                    None => (), }
 }
 
-/// Compare two metrics for equality, tol(0. - 100.) is given in percent!
-fn compare_single(value1: f64, value2: f64, tol: f64) -> Comparison {
-    assert!(value1 != 0.);
-    if (value1 - value2).abs() / value1.abs() <= (tol / 100.) { return Comparison::Equal; }
-    else if value1 < value2 { return Comparison::OneIsFaster; }
-    else { return Comparison::TwoIsFaster; }
-}
-
 #[test]
 fn test_compare_single() {
     match compare_single(1., 1., 0.) {
@@ -218,11 +248,6 @@ fn test_compare_single() {
     }
 }
 
-/// Calculate the procentual variance for that case. 100. * stddev / avg
-pub fn calc_relative_variance(statistics: &BMStatistics) -> f64 {
-    100. * statistics.dev / statistics.avg
-}
-
 #[test]
 fn test_relative_variance() {
     let mut ez_stats = BMStatistics{
@@ -239,25 +264,4 @@ fn test_relative_variance() {
     assert_eq!(calc_relative_variance(&ez_stats), 1.);
 }
 
-/// Read in a result file and return all execution times mapped to their command name.
-/// Panics if the file is not existing or the yaml cant be loaded.
-pub fn read_result_from_file(file_name: &str) -> BTreeMap<String, Vec<f32>> {
-    let yml = file_to_yaml(file_name);
-    let yml = &yml[0];
-    let mut result = BTreeMap::new();
 
-    for single_result in yml.as_vec().unwrap() {
-        let single_result = single_result.as_hash().unwrap();
-
-        for (name, times) in single_result {
-            let mut times_float = Vec::new();
-
-            for el in times.as_vec().unwrap() {
-                times_float.push(el.as_f64().unwrap() as f32);
-            }
-            result.insert(name.as_str().unwrap().to_string(), times_float);
-        }
-    }
-
-    result
-}
