18893c8ef27d730c74e4e85afcb83bd77dd8c360
Jonas Toth <development@jonas-toth.eu>
2018-09-19 12:14:36 +0200

    [Refactor] match results of spawning and finishing the bm process

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -49,9 +49,9 @@ fn convert_duration_to_seconds(dur: Duration) -> f32 {
 /// Start all benchmarks in a threadpool and configure a channel to receive a Report for every
 /// finished run.
 pub fn do_benchmark(pool: &ThreadPool,
-                name: &str,
-                channel_trans: Sender<Report>,
-                config: &RunConfig) {
+                    name: &str,
+                    channel_trans: Sender<Report>,
+                    config: &RunConfig) {
     for _ in 0..config.count {
         // threads need own version of the data
         let name = name.to_string();
@@ -63,17 +63,32 @@ pub fn do_benchmark(pool: &ThreadPool,
 
         pool.execute(move || {
             let start_time = Instant::now();
-            let mut process = Command::new(&cmd)
-                .args(&args)
+            let mut process = Command::new(&cmd);
+            process.args(&args)
                 .stdout(Stdio::null())
                 .stderr(Stdio::null())
-                .current_dir(&dir)
-                .spawn()
-                .expect("Program start failed!");
-            let ecode = process.wait()
-                .expect("Failed to wait on program to finish!");
-            let execution_time = start_time.elapsed();
-            tx.send(Report::new(name, execution_time, ecode)).unwrap();
+                .current_dir(&dir);
+
+            match process.spawn() {
+                Ok(mut child) => {
+                    match child.wait() {
+                        Ok(ecode) =>  {
+                            let execution_time = start_time.elapsed();
+                            tx.send(Report::new(name, execution_time, ecode)).unwrap();
+                        }
+                        Err(_) => {
+                            println!("Program did not finish as expected");
+                            println!("Command: {}", &cmd);
+                            println!("Workdir: {}", &dir);
+                        }
+                    }
+                }
+                Err(_) => {
+                    println!("Could not start benchmark {}", &name);
+                    println!("Command: {}", &cmd);
+                    println!("Workdir: {}", &dir);
+                }
+            }
         });
     }
 }
