79cf8f0c1109c5c8732e4e46aee4d68d368022c9
Jonas Toth <development@jonas-toth.eu>
2019-09-30 10:48:50 +0200

    [Misc] release new patch

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ mod statistics;
 fn main() {
     // ---------------- Configuration for the command line parser
     let matches = App::new("macrobm")
-        .version("v0.4.4")
+        .version("v0.4.5")
         .author("Jonas Toth <development@jonas-toth.eu>")
         .about("Times execution time of commands and produces statistics.")
         .arg(Arg::with_name("config")
